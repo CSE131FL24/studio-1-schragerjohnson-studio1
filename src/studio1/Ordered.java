3b85347edce597c1ce92fd8c32b15bade3e04bb3
dwaynesmac <dwaynesmac@dwaynes-air.dhcp.wustl.edu>
2024-09-05 12:40:29 -0500

    Hello

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,9 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+        boolean is0rdered = x<y && y<z || z<y && y<x;
+        System.out.println(is0rdered);
+      
 	}
 
 }
